2078d4a9a79cf77761e5afb856569ac52690e928
Perveneckas Tomas <tomas.perveneckas@mif.stud.vu.lt>
2020-10-07 15:21:30 +0300

    Populated item and computer classes a bit

--- ItemLibrary/Computer.cs
+++ ItemLibrary/Computer.cs
@@ -4,7 +4,49 @@ using System.Text;
 
 namespace ItemLibrary
 {
-    class Computer : Item
+    public class Computer : Item
     {
+        public string ProcessorName { get; set; }
+        public double ProcessorFrequency { get; set; }
+        public string GraphicsCardName { get; set; }
+        public int GraphicsCardMemory { get; set; }
+        public int RAM { get; set; }
+        public string RAM_type { get; set; }
+        public string Resolution { get; set; }
+        public int StorageCapacity { get; set; }
+
+
+        //builder pattern??
+        public Computer(ulong Code, ulong Barcode, int price, string name, string Manufacturer,
+            string ProcName, double ProcFreq, string GPUname, int GPUmem, int ram, string RAMtype,
+            string resol, int storage)
+        {
+
+            //base class
+            ItemCode = Code;
+            ItemBarcode = Barcode;
+            Price = price;
+            Name = name;
+            ManufacturerName = Manufacturer;
+
+            //derived class
+            ProcessorName = ProcName;
+            ProcessorFrequency = ProcFreq;
+            GraphicsCardName = GPUname;
+            GraphicsCardMemory = GPUmem;
+            RAM = ram;
+            RAM_type = RAMtype;
+            Resolution = resol;
+            StorageCapacity = storage;
+        }
+
+
+        //irgi testavimui
+        public override void PrintOut()
+        {
+            Console.WriteLine(ItemCode + " " + ItemBarcode + " " + Price + " " + Name + " " + ManufacturerName);
+            Console.WriteLine(ProcessorName + " " + ProcessorFrequency + " " + RAM + " " + RAM_type + " " + StorageCapacity);
+            Console.WriteLine(GraphicsCardName + " " + GraphicsCardMemory + " " + Resolution);
+        }
     }
 }
--- ItemLibrary/Item.cs
+++ ItemLibrary/Item.cs
@@ -4,6 +4,19 @@ namespace ItemLibrary
 {
     public abstract class Item
     {
+        public ulong ItemCode { get; set; }
+        public ulong ItemBarcode { get; set; }
+        public int Price { get; set; }
+        public string Name { get; set; }
+        public string ManufacturerName { get; set; }
+
+
+
+        //cia testavimui
+        public virtual void PrintOut()
+        {
+            Console.WriteLine(ItemCode + " " + ItemBarcode + " " + Price + " " + Name + " " + ManufacturerName);
+        }
 
     }
 }
