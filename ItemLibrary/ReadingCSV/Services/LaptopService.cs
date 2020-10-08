1ed6fdacc9624268c5a491aacec431cbe79741ad
Ugnius Staniukynas <ugnius.staniukynas@mif.stud.vu.lt>
2020-10-08 14:25:29 +0300

    updated data, added reading

---
+++ ItemLibrary/ReadingCSV/Services/LaptopService.cs
@@ -0,0 +1,43 @@
+﻿using CsvHelper;
+using ItemLibrary.ReadingCSV.Mappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ItemLibrary.ReadingCSV.Services
+{
+    public class LaptopService
+    {
+        public List<Computer> ReadCsvFile(string path)
+        {
+            try
+            {
+                using (var reader = new System.IO.StreamReader(path, Encoding.Default))
+                using (var csv = new CsvReader(reader))
+                {
+                    csv.Configuration.RegisterClassMap<LaptopMap>();
+                    var records = csv.GetRecords<Computer>().ToList();
+                    return records;
+                }
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                throw new Exception(e.Message);
+            }
+            catch (FieldValidationException e)
+            {
+                throw new Exception(e.Message);
+            }
+            catch (CsvHelperException e)
+            {
+                throw new Exception(e.Message);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
+
