def3e3f8701e2e7095f05ea080622e534155575b
Ugnius Staniukynas <ugnius.staniukynas@mif.stud.vu.lt>
2020-10-11 18:07:59 +0300

    Excluded ReadingCSV from ItemLibrary and added a new project for DataContent

---
+++ DataContent/ReadingCSV/Mappers/LaptopMap.cs
@@ -0,0 +1,27 @@
+﻿using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItemLibrary.ReadingCSV.Mappers
+{
+    public sealed class LaptopMap: ClassMap<Computer>
+    {
+        public LaptopMap()
+        {
+            Map(x => x.Name).Name("laptop_name");
+            Map(x => x.ItemURL).Name("laptop_url");
+            Map(x => x.Price).Name("laptop_price").TypeConverter<DoubleConverter>();
+            Map(x => x.ManufacturerName).Name("laptop_manufacturer");
+            Map(x => x.Resolution).Name("laptop_resolution");
+            Map(x => x.ProcessorName).Name("laptop_processor_class");
+            Map(x => x.RAM_type).Name("laptop_ram_type");
+            Map(x => x.RAM).Name("laptop_ram").TypeConverter<Int32Converter>();
+            Map(x => x.StorageCapacity).Name("laptop_storage").TypeConverter<Int32Converter>();
+            Map(x => x.GraphicsCardName).Name("laptop_graphic_card");
+            Map(x => x.GraphicsCardMemory).Name("laptop_graphic_card_memory");
+
+        }
+    }
+}
