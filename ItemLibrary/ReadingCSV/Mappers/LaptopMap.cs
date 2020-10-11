75d364a1ebeecf0286ccfb37e11aa7bf2a81e09b
Ugnius Staniukynas <ugnius.staniukynas@mif.stud.vu.lt>
2020-10-11 18:04:56 +0300

    Fixed reading from CSV code

--- ItemLibrary/ReadingCSV/Mappers/LaptopMap.cs
+++ ItemLibrary/ReadingCSV/Mappers/LaptopMap.cs
@@ -1,4 +1,5 @@
 ﻿using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,16 +12,16 @@ namespace ItemLibrary.ReadingCSV.Mappers
         {
             Map(x => x.Name).Name("laptop_name");
             Map(x => x.ItemURL).Name("laptop_url");
-            Map(x => x.Price).Name("laptop_price");
+            Map(x => x.Price).Name("laptop_price").TypeConverter<DoubleConverter>();
             Map(x => x.ManufacturerName).Name("laptop_manufacturer");
             Map(x => x.Resolution).Name("laptop_resolution");
             Map(x => x.ProcessorName).Name("laptop_processor_class");
             Map(x => x.RAM_type).Name("laptop_ram_type");
-            Map(x => x.RAM).Name("laptop_ram");
-            Map(x => x.StorageCapacity).Name("laptop_storage");
+            Map(x => x.RAM).Name("laptop_ram").TypeConverter<Int32Converter>();
+            Map(x => x.StorageCapacity).Name("laptop_storage").TypeConverter<Int32Converter>();
             Map(x => x.GraphicsCardName).Name("laptop_graphic_card");
             Map(x => x.GraphicsCardMemory).Name("laptop_graphic_card_memory");
-            
+
         }
     }
 }
--- ItemLibrary/ReadingCSV/Services/LaptopService.cs
+++ ItemLibrary/ReadingCSV/Services/LaptopService.cs
@@ -2,6 +2,8 @@
 using ItemLibrary.ReadingCSV.Mappers;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -13,29 +15,24 @@ namespace ItemLibrary.ReadingCSV.Services
         {
             try
             {
-                using (var reader = new System.IO.StreamReader(path, Encoding.Default))
+                using (var reader = new StreamReader(path))
                 using (var csv = new CsvReader(reader))
                 {
+                    csv.Configuration.CultureInfo = CultureInfo.InvariantCulture;
+                    csv.Configuration.Delimiter = ",";
                     csv.Configuration.RegisterClassMap<LaptopMap>();
                     var records = csv.GetRecords<Computer>().ToList();
+
                     return records;
                 }
             }
-            catch (UnauthorizedAccessException e)
+            catch (FileNotFoundException e)
             {
-                throw new Exception(e.Message);
-            }
-            catch (FieldValidationException e)
-            {
-                throw new Exception(e.Message);
-            }
-            catch (CsvHelperException e)
-            {
-                throw new Exception(e.Message);
+                throw new Exception("File not found");
             }
             catch (Exception e)
             {
-                throw new Exception(e.Message);
+                throw e;
             }
         }
     }
