18094440a13bdad3f8bc09216f980c61d2981932
Perveneckas Tomas <tomas.perveneckas@mif.stud.vu.lt>
2020-10-13 15:15:33 +0300

    Added FilterByName method

--- DataManipulation/Filter.cs
+++ DataManipulation/Filter.cs
@@ -1,8 +1,25 @@
-﻿using System;
+﻿using ItemLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DataManipulation
 {
     public class Filter
     {
+        private List<Item> _items { get; set; }
+
+        public Filter(List<Item> items)
+        {
+            _items = items;
+        }
+
+        //Filters item list by a specific name and sets the private field to the filtered list
+        public List<Item> FilterByName(string name)
+        {
+            List<Item> result = _items.Where(item => item.Name == name).ToList();
+            _items = result;
+            return result;
+        }
     }
 }
