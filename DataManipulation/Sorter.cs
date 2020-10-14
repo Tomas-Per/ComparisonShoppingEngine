3bca15502819fec7ce6c6d60a93cfa578873c682
Perveneckas Tomas <tomas.perveneckas@mif.stud.vu.lt>
2020-10-14 20:24:39 +0300

    Added sorting by item name and price by ascending and descending orders

--- DataManipulation/Sorter.cs
+++ DataManipulation/Sorter.cs
@@ -1,6 +1,7 @@
 ﻿using ItemLibrary;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DataManipulation
@@ -14,5 +15,29 @@ namespace DataManipulation
             _items = items;
         }
 
+        public List<Item> SortByNameAsc()
+        {
+            List<Item> result = _items.OrderBy(x => x.Name).ToList();
+            return result;
+        }
+
+        public List<Item> SortByPriceAsc()
+        {
+            List<Item> result = _items.OrderBy(x => x.Price).ToList();
+            return result;
+        }
+
+
+        public List<Item> SortByNameDesc()
+        {
+            List<Item> result = _items.OrderByDescending(x => x.Name).ToList();
+            return result;
+        }
+
+        public List<Item> SortByPriceDesc()
+        {
+            List<Item> result = _items.OrderByDescending(x => x.Price).ToList();
+            return result;
+        }
     }
 }
