42f706992b0734d672895b620756ee23de7d038e
Ugnius Staniukynas <ugnius.staniukynas@mif.stud.vu.lt>
2020-10-14 17:17:21 +0300

    Added item comparison project and branch

---
+++ Comparison/Comparisons/ComputerComparison.cs
@@ -0,0 +1,35 @@
+﻿using ItemLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Comparison.Comparisons
+{
+    class ComputerComparison : ItemComparison
+    {
+        public int StorageWeight { get; set; }
+        public int RamWeight { get; set; }
+        public ComputerComparison(int priceWeight, int storageWeight, int ramWeight)
+                                    : base(priceWeight)
+        {
+            StorageWeight = storageWeight;
+            RamWeight = ramWeight;
+            TotalWeight += StorageWeight + RamWeight;
+        }
+        public (double, double) StorageComparison (int mainStorage, int comparingStorage)
+        {
+            return SpecComparison(Convert.ToDouble(mainStorage), Convert.ToDouble(comparingStorage), StorageWeight);
+        }
+        public (double, double) RamComparison (int mainRAM, int comparingRAM)
+        {
+            return SpecComparison(Convert.ToDouble(mainRAM), Convert.ToDouble(comparingRAM), RamWeight);
+        }
+        //might delete this if not needed in the future:
+        public void ObjectComparison(Computer mainItem, Computer comparingItem)
+        {
+            PriceComparison(mainItem.Price, comparingItem.Price);
+            StorageComparison(mainItem.StorageCapacity, comparingItem.StorageCapacity);
+            RamComparison(mainItem.RAM, comparingItem.RAM);
+        }
+    }
+}
---
+++ Comparison/ItemComparison.cs
@@ -0,0 +1,43 @@
+﻿using ItemLibrary;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Comparison
+{
+    class ItemComparison
+    {
+        public int PriceWeight { get; set; }
+        public double MainItemRanking {get; set;}
+        public double ComparingItemRanking { get; set; }
+        public int TotalWeight { get; set; }
+
+        public ItemComparison(int priceWeight)
+        {
+            PriceWeight = priceWeight;
+            MainItemRanking = 0;
+            ComparingItemRanking = 0;
+            TotalWeight = PriceWeight;
+        }
+        public (double, double) SpecComparison(double mainSpec, double comparingSpec, int specWeight)
+        {
+            double _sum = mainSpec + comparingSpec;
+            double _mainRating = ((specWeight / TotalWeight) * 100 * mainSpec) / _sum;
+            double _comparingRating = ((specWeight / TotalWeight) * 100 * comparingSpec) / _sum;
+            MainItemRanking += _mainRating;
+            ComparingItemRanking += _comparingRating;
+            return (_mainRating, _comparingRating);
+        }
+
+        public (double, double) PriceComparison(double mainPrice, double comparingPrice)
+        {
+            return SpecComparison(mainPrice, comparingPrice, PriceWeight);
+        }
+        //might delete this if not needed in the future:
+        public void ObjectComparison(Item mainItem, Item comparingItem)
+        {
+            PriceComparison(mainItem.Price, comparingItem.Price);
+        }
+    }
+}
