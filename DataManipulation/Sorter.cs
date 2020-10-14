ddaf1b47e32da4fcdc68572b1de7bba55b5b0ec5
Perveneckas Tomas <tomas.perveneckas@mif.stud.vu.lt>
2020-10-14 20:03:13 +0300

    Added constructor

--- DataManipulation/Sorter.cs
+++ DataManipulation/Sorter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ItemLibrary;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,12 @@ namespace DataManipulation
 {
     class Sorter
     {
+        private List<Item> _items { get; set; }
+
+        public Sorter(List<Item> items)
+        {
+            _items = items;
+        }
+
     }
 }
