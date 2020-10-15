de9c65b4b8342c37e95f766f269553629cd0eda9
lamu6354 <laura.mucinyte@mif.stud.vu.lt>
2020-10-15 22:54:52 +0300

    Added ListView and test items, search bar.

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+
+public class Computer {
+    public string Name { get; set; }
+}
 
 namespace WPF
 {
@@ -25,9 +32,20 @@ namespace WPF
             InitializeComponent();
         }
 
+        Computer test = new Computer();
         private void PriceSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             txtSliderValue.Text = "Price up to: " + PriceSlider.Value.ToString() + "$";
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var computers = new List<Computer>();
+            computers.Add(new Computer() { Name = "Apple - MacBook Air 13.3\"" });
+            computers.Add(new Computer() { Name = "Asus - 14\" ZenBook Duo Touch Laptop" });
+            computers.Add(new Computer() { Name = "Lenovo - IdeaPad 15.6\" Laptop"});
+
+            ItemsListView.ItemsSource = computers;
         }
 
         /*
