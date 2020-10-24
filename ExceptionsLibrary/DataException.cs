635db4cfcffa1eb5ef96967555f001cad2289c05
Ugnius Staniukynas <ugnius.staniukynas@mif.stud.vu.lt>
2020-10-24 18:37:29 +0300

    Created custom exception for data errors

---
+++ ExceptionsLibrary/DataException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionsLibrary
+{
+    class DataException : Exception
+    {
+       public event EventHandler<string> DataError;
+       public DataException(string message, object sender) : base(message)
+        {
+            DataError?.Invoke(sender, message);
+        }
+    }
+}
