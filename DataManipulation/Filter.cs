05a44d056fa057454a00468a5dd52ff6b9ab48e6
Ugniusst <ugnius.staniukynas@mif.stud.vu.lt>
2020-10-27 19:22:35 +0200

    Changed field to protected and delted FilterByName method

--- DataManipulation/Filter.cs
+++ DataManipulation/Filter.cs
@@ -7,19 +7,11 @@ namespace DataManipulation
 {
     public class Filter
     {
-        private List<Item> _items { get; set; }
+        protected List<Item> _items { get; set; }
 
         public Filter(List<Item> items)
         {
             _items = items;
-        }
-
-        //Filters item list by a specific name and sets the private field to the filtered list
-        public List<Item> FilterByName(string name)
-        {
-            List<Item> result = _items.Where(item => item.Name == name).ToList();
-            _items = result;
-            return result;
         }
 
         //Filters item list by the manufacturer and sets the private field to the filtered list
