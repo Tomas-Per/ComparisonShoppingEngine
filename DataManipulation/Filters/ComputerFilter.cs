26ff3624e6464ea8b309f87cbc30b9cbee5d3d50
Ugniusst <ugnius.staniukynas@mif.stud.vu.lt>
2020-10-27 19:39:27 +0200

    Added FilterByProcessor

--- DataManipulation/Filters/ComputerFilter.cs
+++ DataManipulation/Filters/ComputerFilter.cs
@@ -1,6 +1,7 @@
 ﻿using ItemLibrary;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DataManipulation.Filters
@@ -10,6 +11,13 @@ namespace DataManipulation.Filters
         public ComputerFilter(List<Computer> items) : base(items)
         {
 
+        }
+        //Filters item list by the processor and sets the protected field to the filtered list
+        public List<Computer> FilterByProcessor(string processor)
+        {
+            List<Computer> result = _items.Where(item => item.ProcessorName == processor).ToList();
+            _items = result;
+            return result;
         }
     }
 }
