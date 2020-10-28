2d3429ee2ddc7875ee38a374933769ab535dbaa9
Justina Šalukaitė <salukaite@gmail.com>
2020-10-28 14:45:59 +0200

    added ItemCategory enum in new class which is seperated from any logic

---
+++ ItemLibrary/Categories.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItemLibrary
+{
+    class Categories
+    {
+        enum ItemCategory
+        {
+            Computer
+        };
+    }
+}
