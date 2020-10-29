a999e90bf37731ccd186fc5b36ab254d8df83151
Justina Šalukaitė <salukaite@gmail.com>
2020-10-30 01:27:47 +0200

    added comments to SenukaiDataUpdater

--- DataUpdater/SenukaiDataUpdater.cs
+++ DataUpdater/SenukaiDataUpdater.cs
@@ -7,6 +7,8 @@ namespace DataUpdater
 {
     public class SenukaiDataUpdater : IDataUpdater
     {
+
+        //starts parsing senukai.lt laptops and writes parsed data to a file
         public void update()
         {
             IParser<Computer> parser = new SenukaiParser();
