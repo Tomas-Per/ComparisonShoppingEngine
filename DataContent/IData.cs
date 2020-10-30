687302c41c8912969ebe1a59750519e00f19b4c1
Ugniusst <ugnius.staniukynas@mif.stud.vu.lt>
2020-10-30 15:52:22 +0200

    Changed interface not to include path

--- DataContent/IData.cs
+++ DataContent/IData.cs
@@ -6,6 +6,6 @@ namespace DataContent
     public interface IData<T> where T : Item
     {
         public List<T> ReadData(string path);
-        public void WriteCSVFile(string path, List<T> list);
+        public void WriteData(List<T> list);
     }
 }
