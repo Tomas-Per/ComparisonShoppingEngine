6f90180801bcf45ebfc6f4037430727e8005e488
Justina Šalukaitė <salukaite@gmail.com>
2020-11-05 15:06:41 +0200

    updated exceptions

--- ShopParser/Parsing.cs
+++ ShopParser/Parsing.cs
@@ -34,11 +34,11 @@ namespace DataContent
             }
             catch (ArgumentOutOfRangeException)
             {
-                throw; //new DataCustomException("Error happened while parsing int", null);
+                throw;
             }
             catch (Exception e)
             {
-                throw new DataCustomException("Error happened while trying to parse: " + e.Message, ParseDouble(null));
+                throw new Exception("Error happened while trying to parse: " + e.Message);
             }
 
             return Convert.ToDouble(text);
@@ -56,7 +56,7 @@ namespace DataContent
             }
             catch (ArgumentOutOfRangeException)
             {
-                throw; //new DataCustomException("Error happened while parsing int", null);
+                throw;
             }
 
             return Convert.ToInt32(text);
