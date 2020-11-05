647d393a4709df97f9b321180441497cff0707ea
Ugniusst <ugnius.staniukynas@mif.stud.vu.lt>
2020-11-05 19:10:11 +0200

    Added FilterSpec service

---
+++ DataContent/ReadingCSV/Mappers/FilterMap.cs
@@ -0,0 +1,17 @@
+﻿using CsvHelper.Configuration;
+using ItemLibrary;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace DataContent.ReadingCSV.Mappers
+{
+    public sealed class FilterMap : ClassMap<FilterSpec>
+    {
+        public FilterMap()
+        {
+            Map(x => x.Name).Index(0);
+        }
+    }
+}
---
+++ DataContent/ReadingCSV/Services/FiltersServiceCSV.cs
@@ -0,0 +1,83 @@
+﻿using CsvHelper;
+using DataContent.ReadingCSV.Mappers;
+using ExceptionsLogging;
+using ItemLibrary;
+using PathLibrary;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DataContent.ReadingCSV.Services
+{
+    public class FiltersServiceCSV : IData<IEnumerable<FilterSpec>>
+    {
+        public string Path { get; set; }
+        private FileMode Filemode { get; set; }
+        public FiltersServiceCSV(string path)
+        {
+            Path = path;
+            Filemode = FileMode.Append;
+        }
+        public IEnumerable<FilterSpec> ReadData()
+        {
+            try
+            {
+                using (var reader = new StreamReader(Path))
+                using (var csv = new CsvReader(reader))
+                {
+                    csv.Configuration.CultureInfo = CultureInfo.InvariantCulture;
+                    csv.Configuration.Delimiter = ",";
+                    csv.Configuration.RegisterClassMap<FilterMap>();
+                    var records = csv.GetRecords<FilterSpec>();
+
+                    return records.ToList();
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                ExceptionLogger.Log(e);
+                throw;
+            }
+            catch (Exception e)
+            {
+                ExceptionLogger.Log(e);
+                throw new Exception("Something's wrong happened:" + e.Message);
+            }
+        }
+        public void WriteData(IEnumerable<FilterSpec> names)
+        {
+            try
+            {
+                using (var stream = File.Open(Path, Filemode))
+                using (StreamWriter sw = new StreamWriter(stream))
+                using (CsvWriter cw = new CsvWriter(sw))
+                {
+                    foreach (FilterSpec name in names)
+                    {
+                        cw.Configuration.RegisterClassMap<FilterMap>();
+                        cw.WriteField(name);
+                        cw.NextRecord();
+                    }
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                ExceptionLogger.Log(e);
+                throw;
+            }
+            catch (FileLoadException e)
+            {
+                ExceptionLogger.Log(e);
+                throw;
+            }
+            catch (Exception e)
+            {
+                ExceptionLogger.Log(e);
+                throw new Exception("Something's wrong happened:" + e.Message);
+            }
+        }
+    }
+}
