4f0ed16df4734b1c0b1d5bc77c5d78fa3344d6c8
Justina Šalukaitė <salukaite@gmail.com>
2020-11-12 23:01:19 +0200

    added some funcionality in Parser

--- AdminService/AdminService.cs
+++ AdminService/AdminService.cs
@@ -19,6 +19,8 @@ namespace AdminService
         {
             string command;
 
+            new PiguParser().ParseShop();
+
             do
             {
                 command = Console.ReadLine();
--- ShopParser/PiguParser.cs
+++ ShopParser/PiguParser.cs
@@ -1,15 +1,40 @@
 ﻿using ItemLibrary;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using System.Text;
+using static ItemLibrary.Categories;
 
 namespace ShopParser
 {
-    oublic class PiguParser : IParser<Computer>
+    public class PiguParser : IParser<Computer>
     {
+        private readonly string _url = "https://pigu.lt/lt/kompiuteriai/nesiojami-kompiuteriai";
+        private IWebDriver _driver;
+
         public List<Computer> ParseShop()
         {
-            throw new NotImplementedException();
+            List<Computer> data = new List<Computer>();
+
+            var options = new ChromeOptions();
+            options.AddArguments("--headless");
+
+            _driver = new ChromeDriver();
+
+            var elements = _driver.FindElements(By.CssSelector("#productBlock28833675 > div"));
+
+            Console.WriteLine(elements.Count);
+
+            foreach (var item in elements)
+            {
+                Console.WriteLine(item.Text);
+            }
+
+            _driver.Navigate().GoToUrl(_url);
+
+            return data;
         }
 
         public Computer ParseWindow(Computer item)
