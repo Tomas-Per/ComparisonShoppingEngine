5bb18127ea3a68624f4b4046063519bdd7371b16
Justina Šalukaitė <salukaite@gmail.com>
2020-11-12 22:49:26 +0200

    added aditional method in Iparser interface

---
+++ ShopParser/PiguParser.cs
@@ -0,0 +1,20 @@
+﻿using ItemLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopParser
+{
+    oublic class PiguParser : IParser<Computer>
+    {
+        public List<Computer> ParseShop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Computer ParseWindow(Computer item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
