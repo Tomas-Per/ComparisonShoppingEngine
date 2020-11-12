5cacb8db2e9d2aeaebf2e435756be225d1f6a34d
Justina Šalukaitė <salukaite@gmail.com>
2020-11-12 23:59:50 +0200

    updated Pigu parser so it updates all computer fields

--- ShopParser/PiguParser.cs
+++ ShopParser/PiguParser.cs
@@ -1,6 +1,7 @@
 ﻿using ItemLibrary;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using Parsing;
 using System;
 using System.Collections.Generic;
 using System.Net.Http.Headers;
@@ -11,35 +12,97 @@ namespace ShopParser
 {
     public class PiguParser : IParser<Computer>
     {
-        private readonly string _url = "https://pigu.lt/lt/kompiuteriai/nesiojami-kompiuteriai";
+        private readonly string _url = "https://pigu.lt/lt/kompiuteriai/nesiojami-kompiuteriai?page=1";
         private IWebDriver _driver;
 
         public List<Computer> ParseShop()
         {
             List<Computer> data = new List<Computer>();
+            List<String> links = new List<String>();
 
             var options = new ChromeOptions();
             options.AddArguments("--headless");
 
             _driver = new ChromeDriver();
 
-            var elements = _driver.FindElements(By.CssSelector("#productBlock28833675 > div"));
+            for (int i = 1; i <= 3; i++)
 
-            Console.WriteLine(elements.Count);
+            _driver.Navigate().GoToUrl(_url);
+
+            var elements = _driver.FindElements(By.ClassName("cover-link"));
 
             foreach (var item in elements)
             {
-                Console.WriteLine(item.Text);
+                links.Add(item.GetAttribute("href"));
             }
 
-            _driver.Navigate().GoToUrl(_url);
+            foreach (var link in links)
+            {
+                _driver.Navigate().GoToUrl(link);
+
+                Computer computer = new Computer { ItemURL = link, ShopName = "Pigu", ItemCategory = ItemCategory.Computer, ComputerCategory = ComputerCategory.Laptop };
+                data.Add(ParseWindow(computer));
+
+                _driver.Navigate().Back();
+            }
+
 
             return data;
         }
 
-        public Computer ParseWindow(Computer item)
+        public Computer ParseWindow(Computer computer)
         {
-            throw new NotImplementedException();
+            computer.Name = _driver.FindElement(By.TagName("h1")).Text;
+            computer.Price = _driver.FindElement(By.XPath("//meta[@itemprop='price']")).GetAttribute("content").ParseDouble();
+            computer.ImageLink = _driver.FindElement(By.ClassName("media-items-wrap")).FindElement(By.TagName("img")).GetAttribute("src");
+
+
+            var table = _driver.FindElements(By.TagName("td"));
+
+            for (int i = 0; i < table.Count; i++)
+            {
+                if (table[i].Text.Contains("Procesoriaus tipas"))
+                {
+                    computer.ProcessorName = table[i + 1].Text;
+                }
+
+                else if (table[i].Text.Contains("Atminties dydis (RAM)"))
+                {
+                    computer.RAM = table[i + 1].Text.ParseInt();
+                }
+
+                else if (table[i].Text.Contains("raiška"))
+                {
+                    computer.Resolution = table[i + 1].Text;
+                }
+
+                else if (table[i].Text.Contains("Atminties tipas"))
+                {
+                    computer.RAM_type = table[i + 1].Text;
+                }
+
+                else if (table[i].Text.Equals("Vaizdo plokštė"))
+                {
+                    computer.GraphicsCardName = table[i + 1].Text;
+                }
+
+                else if (table[i].Text.Contains("Kietasis diskas SSD") || table[i].Text.Contains("Kietasis diskas HDD"))
+                {
+                    //this line of code will work when we will update ParseInt
+                    //computer.StorageCapacity += table[i + 1].Text.ParseInt(); 
+                }
+
+            }
+
+            
+
+
+
+
+
+
+
+            return computer;
         }
     }
 }
