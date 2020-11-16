c860277fd2788cac93cf1e6e3bd86aa12bbccdf7
Ugniusst <ugnius.staniukynas@mif.stud.vu.lt>
2020-11-16 20:12:26 +0200

    Added id fields

--- ItemLibrary/Processor.cs
+++ ItemLibrary/Processor.cs
@@ -4,6 +4,7 @@ namespace ItemLibrary
 {
     public struct Processor
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Model { get; set; }
         public  int Cache { get; set; }
