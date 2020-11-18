81b8ac436363207c9e635c360fa1010b3128010f
Tomas-Per <tomasperveneckas9@gmail.com>
2020-11-18 18:31:47 +0200

    Added smarphone filter (by processor and screen diagonal)

---
+++ DataManipulation/Filters/SmartphoneFilter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ItemLibrary;
+using Parsing;
+
+namespace DataManipulation.Filters
+{
+    public class SmartphoneFilter : Filter<Smartphone>
+    {
+        public SmartphoneFilter(List<Smartphone> items) : base(items)
+        {
+
+        }
+
+        public List<Smartphone> FilterByProcessor(string processor)
+        {
+            List<Smartphone> result = _items.Where(item => (item.Processor).DeleteSpecialChars() == processor).ToList();
+            return result;
+        }
+
+        public List<Smartphone> FilterByScreenDiagonal(double diagonal)
+        {
+            List<Smartphone> result = _items.Where(item => item.ScreenDiagonal == diagonal).ToList();
+            return result;
+        }
+    }
+}
