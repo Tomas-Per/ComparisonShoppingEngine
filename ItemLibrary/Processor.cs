76bbbab7a445245f473adfa393f88dd2afc4d632
Ugniusst <ugnius.staniukynas@mif.stud.vu.lt>
2020-11-18 20:27:49 +0200

    Added SetName in Processor

--- ItemLibrary/Processor.cs
+++ ItemLibrary/Processor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace ItemLibrary
 {
@@ -14,6 +15,17 @@ namespace ItemLibrary
         public string Model { get; set; }
         public  int Cache { get; set; }
         public int MinCores { get; set; }
+
+        //sets Processor type/name according to it's model
+        public void SetName()
+        {
+            string name;
+            if (Model.Contains("-")) name = Model.Substring(0, Model.IndexOf("-"));
+            else name = Model.Substring(0, Model.IndexOf(Model.Split().Last()));
+            if (name.Contains("Core") || name.Contains("Pentium")) name = "Intel " + name;
+            else if (name.Contains("Ryzen") || name.Contains("Athlon")) name = "AMD " + name;
+            Name = name;
+        }
         
     }
 }
