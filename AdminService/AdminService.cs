2e263f75f1893a32b8047edcc430a90e09a5eba3
Ugniusst <ugnius.staniukynas@mif.stud.vu.lt>
2020-11-19 21:34:06 +0200

    Updated Admin service. Now can update processor in DB

--- AdminService/AdminService.cs
+++ AdminService/AdminService.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.IO;
 using PathLibrary;
 using WebParser.ComponentsParser;
+using DataContent.ReadingDB.Services;
 
 namespace AdminService
 {
@@ -14,6 +15,7 @@ namespace AdminService
     public class AdminService
     {
         public static string _helpMessage = "1 - parse Laptops from Senukai" +
+                                            "5 - Update Processor in database" +
                                             "\n0 - close program";
 
         public static void Main(string[] args)
@@ -39,9 +41,18 @@ namespace AdminService
                         Console.WriteLine("Shop Parsed");
                         break;
                     case "5":
-                        var processor = 
+                        Console.WriteLine("Type processor ID");
+                        var service = new ProcessorDataService();
+                        var processor = service.GetProccesorByID(Int32.Parse(Console.ReadLine()));
+                        Console.WriteLine("Write Name, Model, Cache, Cores");
+                        var specs = Console.ReadLine().Split(',');
+                        processor.Name = specs[0];
+                        processor.Model = specs[1];
+                        processor.Cache = Int32.Parse(specs[2]);
+                        processor.MinCores = Int32.Parse(specs[3]);
+                        service.UpdateProcessor(processor);
+                        break;
 
-                        
                     default:
                         Console.WriteLine("Wrong input, use !help to learn about commands");
                         break;
