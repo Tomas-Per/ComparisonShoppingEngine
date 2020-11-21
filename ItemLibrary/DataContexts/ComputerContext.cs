ee09019c94dd88c1233bfd811044a79d2e7b1aa1
Ugniusst <ugnius.staniukynas@mif.stud.vu.lt>
2020-11-21 15:56:43 +0200

    Updated passord

--- ItemLibrary/DataContexts/ComputerContext.cs
+++ ItemLibrary/DataContexts/ComputerContext.cs
@@ -11,6 +11,6 @@ namespace ItemLibrary.DataContexts
         public DbSet<Computer> Computers { get; set; }
         public DbSet<Processor> Processors { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlServer("Server=tcp:libradb.database.windows.net,1433;Initial Catalog=LibraDb;Persist Security Info=False;User ID=adminlibra;Password={Libra123!};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            => options.UseSqlServer("Server=tcp:libradb.database.windows.net,1433;Initial Catalog=LibraDb;Persist Security Info=False;User ID=adminlibra;Password=Libra123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
     }
 }
