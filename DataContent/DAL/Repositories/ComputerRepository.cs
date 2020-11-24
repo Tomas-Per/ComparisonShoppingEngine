5975e43b623387669ace88d0fe8d4b32c50075d5
Tomas-Per <tomasperveneckas9@gmail.com>
2020-11-25 01:41:17 +0200

    Added computer repo class

---
+++ DataContent/DAL/Repositories/ComputerRepository.cs
@@ -0,0 +1,38 @@
+﻿using DataContent.DAL.Interfaces;
+using ItemLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContent.DAL.Repositories
+{
+    class ComputerRepository : IComputerRepository
+    {
+        public Task<Computer> CreateComputerAsync(Computer computer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Computer> DeleteComputerAsync(Computer computer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Computer>> GetAllComputersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Computer> GetComputerByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Computer> UpdateComputerAsync(int id, Computer computer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
