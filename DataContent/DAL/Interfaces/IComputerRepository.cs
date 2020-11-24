2441967b8ff59c348c76ff46365c5fde141f243a
Tomas-Per <tomasperveneckas9@gmail.com>
2020-11-25 01:37:36 +0200

    Filled computer repository interface

--- DataContent/DAL/Interfaces/IComputerRepository.cs
+++ DataContent/DAL/Interfaces/IComputerRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ItemLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace DataContent.DAL.Interfaces
 {
     interface IComputerRepository
     {
+        Task<List<Computer>> GetAllComputersAsync();
+        Task<Computer> GetComputerByIdAsync(int id);
+        Task<Computer> UpdateComputerAsync(int id, Computer computer);
+        Task<Computer> CreateComputerAsync(Computer computer);
+        Task<Computer> DeleteComputerAsync(Computer computer);
+
     }
 }
