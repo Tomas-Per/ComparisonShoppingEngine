6b0ac709d6f37c7da9e1d9797d028be4762a1843
Tomas-Per <tomasperveneckas9@gmail.com>
2020-11-25 17:16:27 +0200

    Added processor repo interface

---
+++ DataContent/DAL/Interfaces/IProcessorRepository.cs
@@ -0,0 +1,18 @@
+﻿using ItemLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContent.DAL.Interfaces
+{
+    public interface IProcessorRepository
+    {
+        Task<List<Processor>> GetAllProcessorsAsync();
+        Task<Processor> GetProcessorByIdAsync(int id);
+        Task<Processor> UpdateProcessorAsync(Processor processors);
+        Task<List<Processor>> AddProcessorsAsync(List<Processor> processors);
+        Task<Processor> DeleteProcessorAsync(int id);
+    }
+}
