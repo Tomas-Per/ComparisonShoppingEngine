a72c720353b82dcbca6d98a7bf2dc222471cf2dc
Tomas-Per <tomasperveneckas9@gmail.com>
2020-11-25 17:19:26 +0200

    Added processor repo with yet unimplemented methods

---
+++ DataContent/DAL/Repositories/ProcessorRepository.cs
@@ -0,0 +1,38 @@
+﻿using DataContent.DAL.Interfaces;
+using ItemLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContent.DAL.Repositories
+{
+    public class ProcessorRepository : IProcessorRepository
+    {
+        public Task<List<Processor>> AddProcessorsAsync(List<Processor> processors)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Processor> DeleteProcessorAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Processor>> GetAllProcessorsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Processor> GetProcessorByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Processor> UpdateProcessorAsync(Processor processors)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
