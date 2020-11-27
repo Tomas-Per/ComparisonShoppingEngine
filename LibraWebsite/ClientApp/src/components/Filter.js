be6dcd716d830d9aba6894fdd29706a3f6846422
lamu6354 <laura.mucinyte@mif.stud.vu.lt>
2020-11-27 02:25:20 +0200

    Changed text style

---
+++ LibraWebsite/ClientApp/src/components/Filter.js
@@ -0,0 +1,28 @@
+﻿import React, { Component } from 'react';
+import './FilterStyle.css';
+import Slider from './Slider'
+import { Grid } from '@material-ui/core';
+
+
+
+export default class Filter extends Component {
+    render() {
+        return (
+            <div id="mySidenav" class="sidenav">
+                <Grid>
+                    <h5>Filters</h5>
+                </Grid>
+                
+                <p>Price up to:</p>
+                <ul id="slider">
+                    <Slider />
+                </ul>
+                <p>Processor</p>
+                <p>Brand</p>
+                <input type="checkbox" id="BrandAsus" name="Asus" value="Asus"/>
+                    <label for="BrandAsus"> Asus</label>
+            </div>
+        );
+
+    }
+}
