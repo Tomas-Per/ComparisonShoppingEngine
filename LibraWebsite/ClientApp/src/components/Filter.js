9362af199a17e19d3f3a2d66a9f9e2023b7bbc8f
lamu6354 <laura.mucinyte@mif.stud.vu.lt>
2020-12-04 02:12:19 +0200

    Added more brand checkboxes

--- LibraWebsite/ClientApp/src/components/Filter.js
+++ LibraWebsite/ClientApp/src/components/Filter.js
@@ -19,8 +19,25 @@ export default class Filter extends Component {
                 </ul>
                 <p>Processor</p>
                 <p>Brand</p>
-                <input type="checkbox" id="BrandAsus" name="Asus" value="Asus"/>
+                <div class="checkbox">
+                    <input type="checkbox" id="BrandAsus" name="Asus" />
                     <label for="BrandAsus"> Asus</label>
+                    <br />
+                    <input type="checkbox" id="BrandApple" name="Apple" />
+                    <label for="BrandAplee"> Apple</label>
+                    <br />
+                    <input type="checkbox" id="BrandLenovo" name="Levono" />
+                    <label for="BrandLenovo"> Lenovo</label>
+                    <br />
+                    <input type="checkbox" id="BrandAcer" name="Acer" />
+                    <label for="BrandAcer"> Acer</label>
+                    <br />
+                    <input type="checkbox" id="BrandDell" name="Dell" />
+                    <label for="BrandDell"> Dell</label>
+                    <br />
+                    <input type="checkbox" id="BrandHuawei" name="Huawei" />
+                    <label for="BrandHuawei"> Huawei</label>
+                </div>
             </div>
         );
 
