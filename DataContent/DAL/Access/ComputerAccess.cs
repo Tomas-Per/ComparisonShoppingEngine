5f38ac6be9d88b612363e145bfc0d99ef4179909
Tomas-Per <tomasperveneckas9@gmail.com>
2020-12-23 01:50:28 +0200

    Added ComputerAccess and SmartphoneAccess classes

---
+++ DataContent/DAL/Access/ComputerAccess.cs
@@ -0,0 +1,36 @@
+﻿using DataContent.DAL.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using ModelLibrary;
+using Newtonsoft.Json;
+
+namespace DataContent.DAL.Access
+{
+    public class ComputerAccess
+    {
+        private string apiUrl = "https://localhost:44315/";
+        private ApiHelper _apiHelper;
+
+        public ComputerAccess()
+        {
+            _apiHelper = new ApiHelper();
+            _apiHelper.InitializeClient();
+        }
+
+        public async Task PostComputers(List<Computer> computers)
+        {
+            var json = JsonConvert.SerializeObject(computers);
+            var postData = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await _apiHelper.Client.PostAsync(apiUrl + "api/Computers", postData); ;
+            if (response.IsSuccessStatusCode)
+            {
+                return;
+            }
+            else throw new HttpRequestException();
+        }
+    }
+}
---
+++ DataContent/DAL/Access/SmartphoneAccess.cs
@@ -0,0 +1,36 @@
+﻿using DataContent.DAL.Helpers;
+using ModelLibrary;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContent.DAL.Access
+{
+    public class SmartphoneAccess
+    {
+        private string apiUrl = "https://localhost:44315/";
+        private ApiHelper _apiHelper;
+
+        public SmartphoneAccess()
+        {
+            _apiHelper = new ApiHelper();
+            _apiHelper.InitializeClient();
+        }
+
+        public async Task PostSmartphones(List<Smartphone> smartphones)
+        {
+            var json = JsonConvert.SerializeObject(smartphones);
+            var postData = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await _apiHelper.Client.PostAsync(apiUrl + "api/Smartphones", postData); ;
+            if (response.IsSuccessStatusCode)
+            {
+                return;
+            }
+            else throw new HttpRequestException();
+        }
+    }
+}
