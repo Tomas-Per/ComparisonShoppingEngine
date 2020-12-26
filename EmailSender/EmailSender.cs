c798a38a39e7d3885b6d77eab8e01d7fa725cd50
Justina Šalukaitė <salukaite@gmail.com>
2020-12-26 13:31:08 +0200

    added SendEmail method in EmailSender

--- EmailSender/EmailSender.cs
+++ EmailSender/EmailSender.cs
@@ -1,7 +1,30 @@
-﻿
+﻿using System.Net;
+using System.Net.Mail;
+
 namespace EmailSender
 {
     public class EmailSender
     {
+        public void SendEmail(string subject, string htmlMessage, string recipient)
+        {
+            var smtpClient = new SmtpClient(EmailSettings.Default.host)
+            {
+                Port = EmailSettings.Default.port,
+                Credentials = new NetworkCredential(EmailSettings.Default.email, EmailSettings.Default.password),
+                EnableSsl = EmailSettings.Default.ssl,
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(EmailSettings.Default.email),
+                Subject = subject,
+                Body = htmlMessage,
+                IsBodyHtml = true,
+            };
+            mailMessage.To.Add(recipient);
+            smtpClient.Send(mailMessage);
+
+        }
+
     }
 }
