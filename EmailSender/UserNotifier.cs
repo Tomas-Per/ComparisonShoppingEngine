6d417cf6b41f16f1d668938bc754640632e7a581
Justina Šalukaitė <salukaite@gmail.com>
2020-12-27 18:13:07 +0200

    updated UserNotifier: now it notifies all users with specific template and also added GetUsersForNotification it gets users from api who need to be notified

---
+++ EmailSender/UserNotifier.cs
@@ -0,0 +1,41 @@
+﻿using EmailSender.EmailTemplates;
+using ModelLibrary;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace EmailSender
+{
+    public class UserNotifier
+    {
+        private EmailSender _emailSender;
+        private HttpClient _httpClient;
+
+        public UserNotifier()
+        {
+            
+            _emailSender = new EmailSender();
+            _httpClient = new HttpClient();
+        }
+        public void NotifyUsersWhenPriceDropped (Item item, double oldPrice, List<User> users)
+        {
+            users.ForEach(user => _emailSender.SendEmail($"{item.Name} price dropped!", PriceDroppedEmailTemplate.GetPriceDroppedEmailTemplate(item, oldPrice, user), user.Email));
+        }
+
+        public async Task<List<User>> GetUsersForNotification(Item item)
+        {
+            List<User> users = new List<User>();
+
+            var apiUrl = ConfigurationManager.AppSettings["api"] + "api/FavoriteItemUsers/" + item.Id;
+            HttpResponseMessage response;
+            response = await _httpClient.GetAsync(apiUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                users = await response.Content.ReadAsAsync<List<User>>();
+            }
+            return users;
+        }
+
+    }
+}
