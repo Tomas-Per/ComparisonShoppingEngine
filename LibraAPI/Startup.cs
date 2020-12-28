47dbb41764a232443cb898df5bd44754db9790c8
Tomas-Per <tomasperveneckas9@gmail.com>
2020-12-28 19:46:18 +0200

    Changed versioning

--- LibraAPI/Startup.cs
+++ LibraAPI/Startup.cs
@@ -43,7 +43,7 @@ namespace LibraAPI
                      
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1.1", new OpenApiInfo { Title = "LibraAPI", Version = "v1.1" });
+                c.SwaggerDoc("v1.2", new OpenApiInfo { Title = "LibraAPI", Version = "v1.2" });
 
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
@@ -66,7 +66,7 @@ namespace LibraAPI
             }
 
             app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1.1/swagger.json", "LibraAPI v1.1"));
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1.2/swagger.json", "LibraAPI v1.2"));
 
             //app.UseHttpsRedirection();
 
