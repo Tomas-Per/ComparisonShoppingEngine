f5f5f9f6bca0dbaf1aa1c8a692e5741771d12f13
Tomas-Per <tomasperveneckas9@gmail.com>
2021-01-01 20:06:40 +0200

    Added smarphone comparison to controller

--- LibraWebsite/Controllers/ComparisonController.cs
+++ LibraWebsite/Controllers/ComparisonController.cs
@@ -31,5 +31,23 @@ namespace LibraWebsite.Controllers
 
             return compare;
         }
+
+        [HttpPost("SmartphoneComparison/{priceWeight}/{storageWeight}/{ramWeight}/{cameraWeight}")]
+        public async Task<ActionResult<SmartphoneComparisonModel>> SmartphoneComparison(List<Smartphone> smartphones, int priceWeight, int storageWeight, int ramWeight, int cameraWeight)
+        {
+            SmartphoneComparison smartphoneComparison = new SmartphoneComparison(priceWeight, ramWeight, storageWeight, cameraWeight);
+            SmartphoneComparisonModel compare = new SmartphoneComparisonModel();
+            var phone1 = smartphones[0];
+            var phone2 = smartphones[1];
+            smartphoneComparison.UpdateRatings(phone1, phone2,
+                    (ranking) => { compare.PriceRanking1 = ranking.Item1.Truncate2(); compare.PriceRanking2 = ranking.Item2.Truncate2(); },
+                    (ranking) => { compare.StorageRanking1 = ranking.Item1.Truncate2(); compare.StorageRanking2 = ranking.Item2.Truncate2(); },
+                    (ranking) => { compare.RamRanking1 = ranking.Item1.Truncate2(); compare.RamRanking2 = ranking.Item2.Truncate2(); },
+                    (ranking) => { compare.CameraRanking1 = ranking.Item1.Truncate2(); compare.CameraRanking2 = ranking.Item2.Truncate2(); },
+                    (itemRanking) => { compare.ItemRanking1 = itemRanking.Item1.Truncate2(); compare.ItemRanking2 = itemRanking.Item2.Truncate2(); }
+                );
+
+            return compare;
+        }
     }
 }
