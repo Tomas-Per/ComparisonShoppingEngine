11395d5bea21122ab5e9d2ab78d18feacc5bdc1d
lamu6354 <laura.mucinyte@mif.stud.vu.lt>
2021-01-03 17:05:10 +0200

    Fixed bugs

--- LibraWebsite/ClientApp/src/App.js
+++ LibraWebsite/ClientApp/src/App.js
@@ -1,29 +1,26 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
+import ReactDOM from 'react-dom';
+import { Route, Router } from 'react-router';
 import { Layout } from './components/Layout';
 import { Categories } from './components/Categories';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 import { ProductsMenu } from './components/ProductsMenu';
-import { Login } from './components/Login';
-
 import './custom.css'
+
 
 export default class App extends Component {
     static displayName = App.name;
 
 render() {
     return (
-      <Layout>
-        <Route exact path='/' component={Categories} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-        <Route path='/categories' component={Categories} />
-        <Route path='/products/:category/:page' component={ProductsMenu}/>
+        <Layout>
+            <Route exact path='/' component={Categories} />
+            <Route path='/counter' component={Counter} />
+            <Route path='/fetch-data' component={FetchData} />
+            <Route path='/categories' component={Categories} />
+            <Route path='/products/:category/:page' component={ProductsMenu} />
       </Layout>
-       /* <Layout>
-            <Route exact path='/' component={Login}/>
-        </Layout>*/
     );
   }
 }
--- LibraWebsite/ClientApp/src/index.js
+++ LibraWebsite/ClientApp/src/index.js
@@ -10,10 +10,10 @@ const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
+    <BrowserRouter basename={baseUrl}>
         <App />
         <Filter />
-  </BrowserRouter>,
+    </BrowserRouter>,
   rootElement);
 
 registerServiceWorker();
