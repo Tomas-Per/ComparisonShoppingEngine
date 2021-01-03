7a1050674706c4fbe0634e18cf930d7390e0539d
Ugniusst <ugnius.staniukynas@mif.stud.vu.lt>
2021-01-03 22:12:15 +0200

    Added comparison items

--- LibraWebsite/ClientApp/src/App.js
+++ LibraWebsite/ClientApp/src/App.js
@@ -5,6 +5,8 @@ import { Categories } from './components/Categories';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 import { ProductsMenu } from './components/ProductsMenu';
+import { ComparisonTab } from './components/ComparisonTab';
+
 
 
 import './custom.css'
@@ -19,7 +21,8 @@ render() {
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
         <Route path='/categories' component={Categories} />
-        <Route path='/products/:category/:page' component={ProductsMenu}/>
+            <Route path='/products/:category/:page' component={ProductsMenu} />
+            <Route path='/comparison' component={ComparisonTab} />
       </Layout>
     );
   }
